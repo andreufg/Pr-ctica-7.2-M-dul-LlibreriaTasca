bb253caef10f361dd57b288eb7a3f8d09cf66241
Andreu Francés Gallego <andreufgromeral@gmail.com>
2024-02-05 18:06:02 +0100

    arreglado

--- llibreria/models/categoria.py
+++ llibreria/models/categoria.py
@@ -4,5 +4,3 @@ class Categoria(models.Model):
     nom = models.CharField(max_length=255, help_text="Introduïx el nom de la categoria")
     descripcio = models.TextField()
 
-    def __str__(self):
-        return self.nom
